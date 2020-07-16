4fde4cdc6b05cbbbae70e303c49b5830e8947d72
Raw Pong Ghmoa <58883403+q9f@users.noreply.github.com>
2020-07-16 11:32:47 +0200

    add local web3 provider

--- src/prysm.sh
+++ src/prysm.sh
@@ -2,6 +2,7 @@
 
 while true
 do
-  beacon-chain --altona --dev >& /root/gather/prysm.log
+  beacon-chain --altona --dev \
+  --http-web3provider http://127.0.0.1:8545 >& /root/gather/prysm.log
   sleep 10
 done
