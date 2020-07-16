3f209fbc4dcaa929e042c999267e324acb7b5eb9
Raw Pong Ghmoa <58883403+q9f@users.noreply.github.com>
2020-07-16 11:45:02 +0200

    update teku config

--- src/teku.sh
+++ src/teku.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  teku --config-file="/root/altona/teku.yaml" \
+  teku --config-file="/root/bench/src/teku.yaml" \
     --network="altona" &> /root/gather/teku.log
   sleep 10
 done
